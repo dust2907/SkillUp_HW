bcbd1a467dbf1ccc61bd07a7cd986ea3d008c809
dust2907 <d.ustsinovich@gmail.com>
2021-06-06 13:30:26 +0300

    Добавил файл print.py

---
+++ Lesson_3/print.py
@@ -0,0 +1,9 @@
+print('Hello!')
+print('Hello,','how are you?')
+print('Great,','and you?', sep='---')
+print('i am fine')
+print('Ok,' 'goodbye!', end=" ")
+print('Have a good day!')
+print('Goodbye!')
+x = 'Хорошо пообщались'
+print(x)
